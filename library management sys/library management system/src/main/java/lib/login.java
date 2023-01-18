0a9f90eeb664d0895b17fb22a1c87acb308a9ebe
Magan-jyot-kaur <121079235+Magan-jyot-kaur@users.noreply.github.com>
2023-01-18 20:44:25 +0530

    Add files via upload

---
+++ library management sys/library management system/src/main/java/lib/login.java
@@ -0,0 +1,40 @@
+package lib;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * Servlet implementation class login
+ */
+public class login extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public login() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+		PrintWriter out = response.getWriter();
+		String name=request.getParameter("name");
+		String password=request.getParameter("pass");
+		
+		if(name.equalsIgnoreCase("nishu") && password.equalsIgnoreCase("nishu")) {
+			response.sendRedirect("login.html");
+		}
+		else {
+			response.sendRedirect("error.html");
+		}
+	}
+
+
+}
